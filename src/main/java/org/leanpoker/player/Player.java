556e9219e6881a5da235414ef1dd9d43f9e38c33
Sz. N. Bálint <szalaynb93@gmail.com>
2018-02-12 10:39:52 +0100

    goodCardCombos

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -3,11 +3,25 @@ package org.leanpoker.player;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
+
+    static List<String> getGoodHeights() {
+        List<HashMap<String, String>> goodCardCombinations = new ArrayList<>();
+        List<String> goodRanks = new ArrayList<>();
+        String[] ranks = { "A","K","Q","J","10" };
+        for(int i = 0, i < ranks.length, i++){
+            goodRanks.add(ranks[i]);
+        }
+        return goodRanks;
+    }
+
 
     public static int betRequest(JsonElement request) {
         JsonObject obj = request.getAsJsonObject();
