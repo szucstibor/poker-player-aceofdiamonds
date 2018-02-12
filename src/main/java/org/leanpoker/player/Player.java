16b06807ef467f96b2cb08307c2023ec7663fb6a
fruzsinaCsongor <csongor.fruzsina@gmail.com>
2018-02-12 14:27:59 +0100

    object

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -30,36 +30,18 @@ public class Player {
             System.out.println("1");
             System.out.println("1");
             JsonObject object = request.getAsJsonObject();
-//            object = object.getAsJsonObject("players");
-//            JsonObject r = request.getAsJsonObject();
-//            JsonArray requests = r.getAsJsonArray();
             JsonArray jarray = object.getAsJsonArray("players");
-            object = jarray.get(0).getAsJsonObject();
+            JsonObject asd = jarray.get(0).getAsJsonObject();
+            System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ezazgeciobject" + object);
+            System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ezazgecijarray" + jarray);
+            System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ezazgeciasd" + asd);
             System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ezazgeci" + object);
             System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ezazgeci" + object);
             System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ezazgeci" + object);
-            System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ezazgeci" + object);
-            System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ezazgeci" + object);
-            System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ezazgeci" + object);
-            System.out.println("2");
-            System.out.println("2");
-            System.out.println("2");
             JsonArray ourCards;
-            System.out.println("3");
-            System.out.println("3");
-            System.out.println("3");
-            System.out.println("3");
             String card1rank = "0";
             String card2rank = "0";
-            System.out.println("4");
-            System.out.println("4");
-            System.out.println("4");
-            System.out.println("4");
             for (JsonElement req : jarray) {
-                System.out.println("5");
-                System.out.println("5");
-                System.out.println("5");
-                System.out.println("5");
                 if (req.getAsJsonObject().get("hole_cards") != null){
                     ourCards = req.getAsJsonArray();
                     card1rank = ourCards.get(0).getAsJsonObject().get("rank").getAsString();
@@ -77,9 +59,9 @@ public class Player {
                 }
             }
             if (card1rank.equals("0")){
-                return 10;
+                return 1;
             } else if (getGoodHeights().contains(card1rank) && getGoodHeights().contains(card2rank) && card1rank.equals(card2rank)) {
-                return 800;
+                return 500;
             /*} else if (getGoodHeights().contains(card1rank) && getGoodHeights().contains(card2rank)) {
                 return 20;
             } else if (getGoodHeights().contains(card1rank) || getGoodHeights().contains(card2rank)) {
