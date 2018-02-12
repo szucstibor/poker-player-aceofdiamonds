ef332a6f97c0fc2aaf673f7aeb3a6d3af565c921
Szűcs Tibor <job.szucstibor@gmail.com>
2018-02-12 10:39:35 +0100

    Trying to iterate trough json

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -10,8 +10,18 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        JsonObject obj = request.getAsJsonObject();
-        obj.get("");
+        try{
+            JsonArray requests = request.getAsJsonArray("request");
+            List<> ourCards = new ArrayList();
+            for (JsonElement request : requests) {
+                if (request.getAsJsonObject().get("hole_cards") != []){
+                    ourCards.add(request.getAsJsonObject().get("hole_cards"));
+                }
+            }
+            obj.get("");
+        } catch (Exeption e){
+          return 999;
+        }
         return 1000;
     }
 
